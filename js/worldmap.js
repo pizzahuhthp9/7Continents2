aac2e239af82eaeb3dd3229f9b22f3f005403f16
pizza <npwchr.work@gmail.com>
2019-11-21 19:15:00 +0700

    เพิ่มรูป 2อัน + แก้ css & js

--- js/worldmap.js
+++ js/worldmap.js
@@ -1,26 +1,26 @@
 var x = document.getElementById("welcome-page-inside-box");
 var y = document.getElementById("back-btn");
 var z = document.getElementById("seccond-content");
-var bg =document.getElementsByTagName("body");
+var welcomePageContainer = document.getElementById("welcome-page-container");
 
 function hidebox(){
-		x.style.display = "none";
-		y.style.display = "block";
-		z.style.display = "block";
-		x.style.opacity = "0";
-		y.style.opacity = "1";
-		z.style.opacity = "1";
-		document.body.style.backgroundColor= "#a1d1e2";
-
+	x.style.display = "none";
+	y.style.display = "block";
+	z.style.display = "block";
+	x.style.opacity = "0";
+	y.style.opacity = "1";
+	z.style.opacity = "1";
+	document.body.style.backgroundColor= "#a1d1e2";
+	welcomePageContainer.style.backgroundImage = "url(image/All-Recovered-No-Star.png)";
 }
 
 function showbox(){
-		x.style.display = "block";
-		y.style.display = "none";
-		z.style.display = "none";
-		x.style.opacity = "1";
-		y.style.opacity = "0";
-		z.style.opacity = "0";
-		document.body.style.backgroundColor= "#2a443f";
-
+	x.style.display = "block";
+	y.style.display = "none";
+	z.style.display = "none";
+	x.style.opacity = "1";
+	y.style.opacity = "0";
+	z.style.opacity = "0";
+	document.body.style.backgroundColor= "#2a443f";
+	welcomePageContainer.style.backgroundImage = "url(image/All-Recovered.png)";
 }
